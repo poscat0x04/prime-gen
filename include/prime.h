ea13356a6ca8d5dac803dffc60382dddc63354bb
Poscat <poscat@poscat.moe>
2021-11-05 11:29:04 +0800

    feat: generate random bigints with a specific length in bits

--- include/prime.h
+++ include/prime.h
@@ -33,6 +33,9 @@ bool BN_mod_exp_mont(BIGINT *r, const BIGINT *a, const BIGINT *e, const MONT_PAR
 void seed(void);
 u64 next(void);
 RNG_PARAMS *to_rng_params(const BIGINT *a, bool copy);
+bool bn_fill_bits(BIGINT *r, int bits);
+RNG_PARAMS *bn_gen_bits_params(int bits);
+bool bn_gen_bits(BIGINT *r, int bits, RNG_PARAMS *params);
 bool bn_gen(BIGINT *r, const RNG_PARAMS *params);
 bool bn_gen_pred(BIGINT *r, const RNG_PARAMS *params, bool (*pred)(const BIGINT *));
 
--- src/prime_rand.c
+++ src/prime_rand.c
@@ -67,13 +67,18 @@ RNG_PARAMS *to_rng_params(const BIGINT *a, bool copy) {
 }
 
 bool bn_gen(BIGINT *r, const RNG_PARAMS *params) {
+  assert(params->hd_used_bits <= 64);
   if (bn_expand_nocpy(r, params->top) == NULL)
     return false;
 
-  u64 m;
+  u64 m, mod;
   int i;
+  mod = UINT64_C(1) << params->hd_used_bits;
 beginning:
-  while ((m = next() & ((1 << params->hd_used_bits) - 1)) > params->d[params->top - 1]);
+  if (params->hd_used_bits == 64)
+    while ((m = next()) > params->d[params->top - 1]);
+  else
+    while ((m = next() & (mod - 1)) > params->d[params->top - 1]);
   r->d[params->top - 1] = m;
   // m == highest digit
   for (i = params->top - 2; i >= 0; i--) {
@@ -101,5 +106,49 @@ bool bn_gen_pred(BIGINT *r, const RNG_PARAMS *params, bool (*pred)(const BIGINT 
     if (!bn_gen(r, params))
       return false;
   } while (!pred(r));
+  return true;
+}
+
+bool bn_fill_bits(BIGINT *r, int bits) {
+  int w = (bits + 63) / 64;
+  r->top = w;
+  if (bn_expand_nocpy(r, w) == NULL)
+    return false;
+  u64 mod = bits % 64;
+  if (mod) {
+    for (int i = 0; i < w - 1; i++) {
+      r->d[i] = UINT64_MAX;
+    }
+    mod = UINT64_C(1) << mod;
+    r->d[w - 1] = UINT64_MAX % mod;
+  } else {
+    for (int i = 0; i < w; i++) {
+      r->d[i] = UINT64_MAX;
+    }
+  }
+  return true;
+}
+
+RNG_PARAMS* bn_gen_bits_params(int bits) {
+  if (bits <= 0)
+    return NULL;
+  BN_init(tmp)
+  bn_fill_bits(tmp, bits);
+  return to_rng_params(tmp, true);
+}
+
+/// Generate a random bigint with exactly n bits
+/// \param r The bigint to receive the result
+/// \param bits The number of bits in the bigint
+/// \param params The rng param given by bn_gen_bits_params
+/// \return Whether the operation nis successful
+bool bn_gen_bits(BIGINT *r, int bits, RNG_PARAMS *params) {
+  int w = (bits + 63) / 64;
+  bits = bits % 64;
+  if (!bn_gen(r, params)
+      || bn_wexpand(r, w) == NULL)
+    return false;
+  u64 mask = UINT64_C(1) << (bits - 1);
+  r->d[r->top - 1] |= mask;
   return true;
 }
